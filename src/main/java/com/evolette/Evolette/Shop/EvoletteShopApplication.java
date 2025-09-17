0db74b7d0fc6d92a750993375ca2400905072a5f
Smita Parida <smitaparida182005@gmail.com>
2025-09-17 13:24:06 +0530

    Initial commit: Spring Boot setup

---
+++ src/main/java/com/evolette/Evolette/Shop/EvoletteShopApplication.java
@@ -0,0 +1,13 @@
+package com.evolette.Evolette.Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoletteShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoletteShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evolette/Evolette/Shop/EvoletteShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evolette.Evolette.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvoletteShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
