ed85ddf82675e34bd1b9525d280751071d2726c9
Smita Parida <smitaparida182005@gmail.com>
2025-10-31 19:01:43 +0530

    Update dashboard with product area.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,44 @@
 import React from "react";
 import "../Dashboard.css";
+import { Link } from "react-router-dom";
+
 
 function Dashboard() {
   return (
-    <div className="dashboard">
-      <h1>Evolette</h1>
-      <p>Handcrafted crochet pieces made with love and detail.</p>
-    </div>
+      <div className="dashboard">
+        <div className="intro">
+          <h1>Evolette</h1>
+          <p>
+            Handcrafted crochet pieces<br />
+            made with love<br />
+            and detail.
+          </p>
+        </div>
+        <Link to="/products" className="card-container">
+                <div className="card">
+                  <img src="/images/bow.jpeg" alt="Crochet Hat" />
+                  <h3>Cozy Crochet Hat</h3>
+                  <p>Soft, warm, and handmade with love.</p>
+                </div>
+
+                <div className="card">
+                  <img src="/images/clip.jpeg" alt="Crochet Bag" />
+                  <h3>Boho Crochet Bag</h3>
+                  <p>Perfect for your everyday essentials.</p>
+                </div>
+
+                <div className="card">
+                  <img src="/images/earrings.jpeg" alt="Crochet Toy" />
+                  <h3>Cute Crochet Toy</h3>
+                  <p>Wholesome gifts for little hearts.</p>
+                </div>
+                <div className="card">
+                     <img src="/images/earrings.jpeg" alt="Crochet Toy" />
+                     <h3>Cute Crochet Toy</h3>
+                     <p>Wholesome gifts for little hearts.</p>
+                </div>
+              </Link>
+      </div>
   );
 }
 
