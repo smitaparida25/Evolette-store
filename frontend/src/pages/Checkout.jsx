0cf8f1ce11d691363dc2e07c0e84d4c85c3da3bc
Smita Parida <smitaparida182005@gmail.com>
2026-03-11 21:22:19 +0530

    Add checkout page with shipping form, UPI payment option, and order summary(hardcoded) layout

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -11,9 +11,46 @@ function Checkout() {
                     <input type="text" placeholder="State" />
                     <input type="text" placeholder="Pincode" />
                 </form>
+
+                <h2> Payment Method </h2>
+                    <div className="payment-method">
+                        <label>
+                            <input type="radio" name="payment" value="upi"/>
+                            Pay with UPI
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Enter UPI ID (example@upi)"
+                                className="upi-input"
+                              />
+                        </div>
             </div>
             <div className="summary">
-                <h2>Order Summary</h2>
+              <h2>Order Summary</h2>
+
+              <div className="item">
+                <span>Crochet Top × 1</span>
+                <span>₹1200</span>
+              </div>
+
+              <div className="line"></div>
+
+              <div className="price-row">
+                <span>Subtotal</span>
+                <span>₹1200</span>
+              </div>
+
+              <div className="price-row">
+                <span>Shipping</span>
+                <span>₹50</span>
+              </div>
+
+              <div className="price-row total">
+                <span>Total</span>
+                <span>₹1250</span>
+              </div>
+
+              <button className="place-order">Place Order</button>
             </div>
         </div>
         );
