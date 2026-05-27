c5b2d5e142dcd3470d4606f9c1717578d8e55253
Smita Parida <smitaparida182005@gmail.com>
2026-05-27 14:00:40 +0530

    convex connection is made and signup route is updated for handling users.

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import { ConvexHttpClient } from "convex/browser";
 import { api } from "../convex/_generated/api.js";
 
 dotenv.config();
 
 const app = express();
+const convex = new ConvexHttpClient(process.env.CONVEX_URL);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,8 +25,21 @@ app.get("/", (req, res) =>{
 });
 
 app.post("/signup", (req, res) =>{
-    res.json({ message: "signup route ready" });
-})
+    try{
+    const{name, email, password} = req.body;
+    const user = await convex.action(api.authActions.signupUser, {
+          email,
+          password,
+          name,
+        });
+        res.json(user);
+    }
+    catch{
+    res.status(400).json({
+          error: error.message,
+        });
+    }
+});
 
 app.post("/login", (req,res) => {
     res.json({ message: "login route ready" });
