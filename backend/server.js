3e831378a295002e2e75b760567f45f7be99b290
Smita Parida <smitaparida182005@gmail.com>
2026-05-28 15:41:50 +0530

    setup express backend with convex auth and jwt cookies

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import { ConvexHttpClient } from "convex/browser";
 import { api } from "../convex/_generated/api.js";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
@@ -24,7 +25,7 @@ app.get("/", (req, res) =>{
     res.send("Backend running")
 });
 
-app.post("/signup", (req, res) =>{
+app.post("/signup", async (req, res) =>{
     try{
     const{name, email, password} = req.body;
     const user = await convex.action(api.authActions.signupUser, {
@@ -34,15 +35,51 @@ app.post("/signup", (req, res) =>{
         });
         res.json(user);
     }
-    catch{
+    catch(error){
     res.status(400).json({
           error: error.message,
         });
     }
 });
 
-app.post("/login", (req,res) => {
-    res.json({ message: "login route ready" });
+app.post("/login", async (req,res) => {
+    try{
+    const {email, password} = req.body;
+
+    const user = await convex.action(api.authActions.loginUser,{
+    email,
+    password,
+    });
+
+    const token = jwt.sign(
+    {
+        userId: user._id,
+        email: user.email,
+    },
+    process.env.JWT_SECRET,
+    {
+    expiresIn: "7d",
+    }
+    );
+
+    res.cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: "strict",
+          maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+
+        res.json({
+          message: "Login successful",
+          user,
+        });
+
+    }
+    catch(error){
+        res.status(400).json({
+              error: error.message,
+            });
+    }
 })
 
 const PORT = 4000;
